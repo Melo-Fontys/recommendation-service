b483e3569f0341b8f0d74fb395e49a9cdf34c39d
Alexandra Ioan <alexandra@eitr.co>
2023-04-06 13:12:53 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const {randomBytes} = require('crypto')
+const bodyParser = require("body-parser")
+const {post} = require("axios");
+const cors = require("cors");
+const app = express();
+const axios = require("axios");
+
+app.use(bodyParser.json())
+app.use(cors())
+
+const recommendations = {};
+
+app.get('/recommendations', (req, res) => {
+    res.send(recommendations)
+})
+
+app.post('/recommendations', async(req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const {title} = req.body;
+
+    recommendations[id] = {id, title};
+
+    await axios.post("http://localhost:8005/events", {
+        type: "RecommendationCreated",
+        data: {
+            id, title
+        }
+    })
+
+    res.status(201).send(recommendations[id])
+})
+
+app.post('/events', (req, res) =>{
+   console.log("Event received", req.body.type);
+
+   res.send({})
+});
+
+app.listen(8000, () => {
+    console.log("Listening on 8000")
+})
